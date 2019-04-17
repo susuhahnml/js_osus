68976eb6bf0ff66323bda0e934428f1f6ce2f16c
Susana Hahn <susuhahnml@yahoo.com.mx>
2019-04-17 23:45:49 +0200

    Example html with js

---
+++ js_html_example/script.js
@@ -0,0 +1,17 @@
+function animate_string(id)
+{
+  console.log("Hola")
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function ()
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
+
+
+
+console.log("Hola")
