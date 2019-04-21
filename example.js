179685cd29383d734a5f9accd4544acfbf091300
Susana Hahn <susuhahnml@yahoo.com.mx>
2019-04-21 23:33:38 +0200

    Examples

---
+++ example.js
@@ -0,0 +1,25 @@
+var person = {name:"Oscar",age:30}
+person.height = 1.75
+console.log(person.height)
+console.log(typeof(person.name))
+
+var a = 1 + "te amo";
+console.log(a*3)
+
+if([]){
+  console.log("Me encantas")
+}
+
+const miVida = "Oscar"
+miVida = ":P"
+
+let miEdad = 25
+miEdad = ":P"
+
+function suma( x , y){
+	console.info('Suma ',x , ' con ' , y);
+	return x+y;
+  console.log('Despes de return')
+}
+
+console.log(suma(1))
